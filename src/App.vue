fc6ead765bff639a8df2ea693aae761d16ae33bc
gesazumoo <gesazumoo@huinnoaim.com>
2025-01-14 16:02:42 +0900

    eslint

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
-  <h2>Hi Vue.js</h2>
-  <h1>Hello Github</h1>
+  <div>
+    <h2>Hi Vue.js</h2>
+    <h1>Hello Github</h1>
+  </div>
 </template>
 
 <script>
 export default {
   name: "App",
-};
+}
 </script>
 
 <style></style>

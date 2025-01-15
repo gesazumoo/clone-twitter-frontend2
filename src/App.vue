6a24507ec7254237e550685ceb0bb7e025aaade8
gesazumoo <gesazumoo@huinnoaim.com>
2025-01-15 15:24:08 +0900

    #3 component styling

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="container">
     <LoginPage />
   </div>
 </template>
@@ -12,4 +12,18 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+body {
+  background:black;
+  color: white;
+}
+
+.container {
+  height: 100vh;
+  display: flex;
+  flex-direction:column;
+  justify-content: center;
+  align-items: center;
+}
+
+</style>

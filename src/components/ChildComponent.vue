12c05d976fa58a301aea1e82e9b18262fce35d2b
gesazumoo <gesazumoo@huinnoaim.com>
2025-01-15 15:59:07 +0900

    props, emit 예제

---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <div>
+            testProps: {{ testProps }}
+        </div>
+        <button @click="sendEmitEvent">Emit!</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ChildComponent",
+    props : {
+        testProps: String
+    },
+    methods: {
+        sendEmitEvent() {
+            this.$emit("fromChildComponent", true)
+        }
+    },
+    emits: ["fromChildComponent"],
+}
+</script>
+
+<style>
+
+</style>

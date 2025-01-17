ff819700e40af12e9cdfca74376a53529a742c04
gesazumoo <gesazumoo@huinnoaim.com>
2025-01-18 00:09:39 +0900

    아이템 빼기

---
+++ src/components/FeedItem.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="feed-container">
+      <div class="feed-header">
+        <div class="feed-content">안녕하세요</div>
+        <button class="feed-delete-button">X</button>
+      </div>
+      <div class="feed-name">김유져</div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+.feed-container {
+    height: 80px;
+    background-color: white;
+    margin: 10px 0px;
+    color: black;
+    padding: 3%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    border-radius: 10px;
+}
+
+.feed-header {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+}
+
+.feed-content {
+    padding: 1%;
+}
+
+.feed-delete-button {
+    background:none;
+    border: none;
+    cursor: pointer;
+}
+
+.feed-name {
+    text-align: right;
+    font-size: 12px;
+}
+</style>
